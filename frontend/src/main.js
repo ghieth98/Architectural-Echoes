62c1cfad768eaa0c5af18b724ba4f3958b337b40
hossam ghieth <hossamghieth@gmail.com>
2023-12-07 00:56:25 +0200

    Added front template

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,16 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import "./assets/style.css";
+import App from "./App.vue";
+import VueSmoothScroll from "vue3-smooth-scroll";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import Button from "./components/Button.vue";
+import LinkButton from "./components/LinkButton.vue";
 
-createApp(App).mount('#app')
+let app = createApp(App);
+
+app.use(VueSmoothScroll);
+
+app.component("Button", Button);
+app.component("LinkButton", LinkButton);
+
+app.mount("#app");
