b25799fd483a056835eed1d489446ad8a429c2c8
hossam ghieth <hossamghieth@gmail.com>
2023-12-20 12:08:42 +0200

    Added loading fallback

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,19 @@
 <script setup>
 
-import Header from './layouts/Header.vue';
-import Main from './layouts/Main.vue';
-import Footer from './layouts/Footer.vue';
+import Header from './sections/Header.vue'
+import Main from './views/Home.vue'
+import Footer from './sections/Footer.vue'
 
 </script>
 
 <template>
-  <Header/>
-
-  <Main/>
+  <Header />
+  <Suspense>
+    <RouterView />
+    <template #fallback>
+      <p>Loading...</p>
+    </template>
+  </Suspense>
 
   <Footer />
 </template>
